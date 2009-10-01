a572d5806ad7549481b74ef327f944bac5210a9a
Jacob Rhoden <jrhoden@unimelb.edu.au>
2009-10-01 16:37:16 +1000

    Build the new JAR file

--- src/org/uptecs/email/examples/SendEmail.java
+++ src/org/uptecs/email/examples/SendEmail.java
@@ -6,15 +6,15 @@ public class SendEmail {
 
 	public static void main(String arg[]) {
 
-		Mail mail = new Mail("smtp.unimelb.edu.au",25);
+		Mail mail = new Mail("localhost",4020);
 		int result = mail.sendHtmlMail(
 				"from@example.com",
-				"jacob@rhoden.id.au",
+				"to@example.com",
 				"Sample test message",
 				"This <a name=\"Also link\">is also</a> a very simple <b>test</b> message. It has some more slightly <a href=\"http://complicated.com/\">complicated</a> html in it It should also be very long and have not many breaks in it.\n\n<br/>Best regards,\nJacob\n\n\n=-==============-=========-===================-========-===============-============-========-=========-=========",
 				"This is a very simple sample\ntest email message.\n\nBest regards,\nJacob"
 				);
-		
+
 		if(result!=0)
 			System.err.println(mail.getError());
 	}
